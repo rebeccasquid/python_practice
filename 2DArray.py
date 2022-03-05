7a42193587fa128f8a49acec017bca9a6951890a
Rebecca <rebecca860903@gmail.com>
2022-03-05 14:55:08 +0800

    newlesson

---
+++ 2DArray.py
@@ -0,0 +1,24 @@
+array1=[1,3,5,7,9]
+print(array1)
+# [1,3,5,7,9]
+
+for row in array1:
+    print(row)
+# 1
+# 3 ...
+
+array2D=[
+    [0,1,2],
+    [3,4,5]
+]
+print('array2D:', array2D)
+# [ [0,1,2],[3,4,5] ]
+
+for row in array2D:
+    print('loop print array2D :',row)
+
+# [0,1,2]
+# [3,4,5]
+
+str = "Hi, Hello"
+print(str)
