2bfd7a75c35ad631ec17e2d6df3213427e29128d
Rebecca <rebecca860903@gmail.com>
2022-03-05 13:27:37 +0800

    first commit

---
+++ first_python.py
@@ -0,0 +1,23 @@
+
+from math import*
+print(round(4.4))
+print(floor(4.6))
+phrase=["hello my name is Rebecca"]
+print(phrase[6:])
+friends=["元","熊","余"]
+
+phrase.extend(friends)
+print(phrase)
+
+def hello():
+    print('hello')
+hello()
+
+def add(num1,num2):
+    print(num1+num2)
+add(2,3)
+
+
+def add(num1,num2):
+    print(num1+num2)
+add(3,4)
