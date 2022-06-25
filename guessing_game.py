e4fe297860fef7ba566d6d24fd6892c4cf3be50b
Rebecca <rebecca860903@gmail.com>
2022-06-25 19:42:39 +0800

    frank modify

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,23 @@
 import random
-num = random.randint(1,100)
+ans = random.randint(1,100)
 guesses = [0]
+
+def printGuessTip(num, guesses):
+    # 拿當前的猜測數字跟前一次猜測數字作比較較接近顯示 WARMER, 叫遠離顯示COLDER
+    # 如果這是第一次猜測, 根據第一次猜測給予提示往上猜或是往下猜
+    if guesses[-2]:  
+        if abs(num-guess) < abs(num-guesses[-2]):
+            print('你猜測的數字比上一次猜測更接近了')
+        elif abs(num-guess) == abs(num-guesses[-2]):
+            print('Try different guess')
+        else:
+            print('COLDER than previous guess!!')
+    else:
+        if num-guess > 0:
+            print('try higher number!')
+        else:
+            print('try lower number!')
+
 
 while True:
 
@@ -12,7 +29,7 @@ while True:
         continue
     
     # here we compare the player's guess to our number
-    if guess == num:
+    if guess == ans:
         print(f'CONGRATULATIONS, YOU GUESSED IT IN ONLY {len(guesses)} GUESSES!!')
         break
         
@@ -22,15 +39,16 @@ while True:
     # when testing the first guess, guesses[-2]==0, which evaluates to False
     # and brings us down to the second section
     
-    if guesses[-2]:  
-        if abs(num-guess) < abs(num-guesses[-2]):
-            print('WARMER!')
-        else:
-            print('COLDER!')
+    # if guesses[-2]:  
+    #     if abs(num-guess) < abs(num-guesses[-2]):
+    #         print('WARMER!')
+    #     else:
+    #         print('COLDER!')
    
-    else:
-        if abs(num-guess) <= 10:
-            print('WARM!')
-        else:
-            print('COLD!')
+    # else:
+    #     if num-guess > 0:
+    #         print('Guess higher!')
+    #     else:
+    #         print('Guess lower!')
 
+    printGuessTip(ans, guesses)
